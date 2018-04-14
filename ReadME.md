d8abfa50534827e0cb6886e6d1060c7dd0a8b252
Biró István <biroistv@outlook.hu>
2018-04-14 13:22:16 +0200

    added two audio controlling button

--- ReadME.md
+++ ReadME.md
@@ -2,7 +2,15 @@
 
 This android application is a simple audio player app. The project is part of the "**Udacity Android Basic Nanodegre** - _Activity Lifecycle and Audio Playbacking_" lesson.
 
-## Info
+The app contains a play and a pause button. which are controlling the mp3 audio file from the res/raw folder.
+
+## Audio
+
+The music from: www.bensound.com
+* Name: [BETTER DAYS](https://www.bensound.com/royalty-free-music/track/better-days) 
+
+
+## Pre-Requiments
 
 * Android SDK Version: 27
 * Minimum SDK Version: 15
--- app/src/main/java/com/example/biro/udacitygdcsabnd_littleaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/biro/udacitygdcsabnd_littleaudioplayer/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.biro.udacitygdcsabnd_littleaudioplayer;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.bensound_betterdays);
     }
+
+    public void playAudio(View view)
+    {
+        this.mp.start();
+    }
+
+    public void pauseAudio(View view)
+    {
+        this.mp.pause();
+    }
+
 }
